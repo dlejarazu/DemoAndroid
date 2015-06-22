6c85d6ebbf17c2b43af4c14ba5bb3a1ca5f5ccae
dlejarazu <dlejarazu@gmail.com>
2015-06-22 20:06:29 +0200

    Solved failing tests for mapPresenter

--- logic/src/test/java/com/diego/civpocket/tests/MapPresenter/MapPresenterBotonesPoblacionLogica.java
+++ logic/src/test/java/com/diego/civpocket/tests/MapPresenter/MapPresenterBotonesPoblacionLogica.java
@@ -1,10 +1,12 @@
 package com.diego.civpocket.tests.MapPresenter;
 
+import com.diego.civpocket.logic.CivPocketGame;
 import com.diego.civpocket.logic.MapPresenter;
 import com.google.inject.Guice;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.*;
 
 import org.junit.Test;
 
@@ -12,25 +14,25 @@ public class MapPresenterBotonesPoblacionLogica extends MapPresenterTester {
 
 	@Test
 	public void testCheckButtonsStatusDuringPhases() {
-		MapPresenter sut = Guice.createInjector(new TestPresenterModule())
-				.getInstance(MapPresenter.class);
 		//StartGame
+		sut.actionSelectRegion("testRegion");
+		given(testGame.getActualPhase()).willReturn(CivPocketGame.GamePhase.StartGame);
 		assertThat(sut.isAddTribeActive(), is(false));
 		assertThat(sut.isMoveTribeActive(), is(false));
-		sut.accionPasarSiguienteFase();
 		//Growth
-		assertThat(sut.isAddTribeActive(), is(true));
+		given(testGame.getActualPhase()).willReturn(CivPocketGame.GamePhase.Growth);
+		assertThat(sut.isAddTribeActive(), is(false));
 		assertThat(sut.isMoveTribeActive(), is(true));
-		sut.accionPasarSiguienteFase();
 		//Events
+		given(testGame.getActualPhase()).willReturn(CivPocketGame.GamePhase.Events);
 		assertThat(sut.isAddTribeActive(), is(false));
 		assertThat(sut.isMoveTribeActive(), is(false));
-		sut.accionPasarSiguienteFase();
 		//Advances
+		given(testGame.getActualPhase()).willReturn(CivPocketGame.GamePhase.Advances);
 		assertThat(sut.isAddTribeActive(), is(false));
 		assertThat(sut.isMoveTribeActive(), is(false));
-		sut.accionPasarSiguienteFase();
 		//Upkeep
+		given(testGame.getActualPhase()).willReturn(CivPocketGame.GamePhase.Upkeep);
 		assertThat(sut.isAddTribeActive(), is(false));
 		assertThat(sut.isMoveTribeActive(), is(false));
 
--- logic/src/test/java/com/diego/civpocket/tests/MapPresenter/MapPresenterTester.java
+++ logic/src/test/java/com/diego/civpocket/tests/MapPresenter/MapPresenterTester.java
@@ -5,6 +5,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import com.diego.civpocket.logic.City;
 import com.diego.civpocket.logic.CityBuilder;
 import com.diego.civpocket.logic.CivPocketGame;
 import com.diego.civpocket.logic.Scenario;
@@ -12,11 +13,8 @@ import com.diego.civpocket.logic.Empire;
 import com.diego.civpocket.logic.MapPresenter;
 import com.diego.civpocket.logic.MapUpdater;
 import com.diego.civpocket.logic.Region;
-import com.diego.civpocket.tests.FakeCityBuilder;
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Inject;
-import com.google.inject.Provides;
 import com.google.inject.testing.fieldbinder.Bind;
 import com.google.inject.testing.fieldbinder.BoundFieldModule;
 
@@ -28,18 +26,10 @@ public class MapPresenterTester {
     @Bind protected Scenario testScenario = mock(Scenario.class);
     @Bind protected Empire testEmpire = mock(Empire.class);
     @Bind protected MapUpdater testView = mock(MapUpdater.class);
-    @Bind protected Region testRegion;
+    @Bind protected CityBuilder testBuilder= mock(CityBuilder.class);
+    protected Region testRegion;
 
     @Inject protected MapPresenter sut;
-
-    protected class TestPresenterModule extends AbstractModule {
-		@Override
-		public void configure() {
-		}
-		@Provides CityBuilder provideFakeBuilder(Empire empire){
-			return new FakeCityBuilder(empire);
-		}
-	}
 
     @Before
     public void setup(){
